ecadfa7f364587e83d2739b0b8f3f96f3706f8d4
ogous <oguzxan.ali@gmail.com>
2024-02-15 17:55:20 +0300

    build: add fly.io config for next.js

--- webapp/next.config.mjs
+++ webapp/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   env: {
     API_URL: process.env.API_URL,
   },
--- webapp/src/app/coin/[id]/layout.js
+++ webapp/src/app/coin/[id]/layout.js
@@ -1,16 +1,16 @@
-export async function generateStaticParams() {
-  const baseUrl = process.env.API_URL
-  if (!baseUrl) throw new Error('API_URL is missing')
-  const res = await fetch(`${baseUrl}/coins`, {
-    method: 'POST',
-  })
-  if (!res.ok) throw new Error(`${baseUrl}/coins could not be fetched`)
-  const data = await res.json()
+// export async function generateStaticParams() {
+//   const baseUrl = process.env.API_URL
+//   if (!baseUrl) throw new Error('API_URL is missing')
+//   const res = await fetch(`${baseUrl}/coins`, {
+//     method: 'POST',
+//   })
+//   if (!res.ok) throw new Error(`${baseUrl}/coins could not be fetched`)
+//   const data = await res.json()
 
-  return data.map((item) => ({
-    id: item.id,
-  }))
-}
+//   return data.map((item) => ({
+//     id: item.id,
+//   }))
+// }
 
 export default function Layout({ children }) {
   return children
